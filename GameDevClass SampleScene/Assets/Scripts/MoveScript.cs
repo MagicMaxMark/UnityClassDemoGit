45e61374355f55df5ec6ec6e950f32356d3e6a5d
magicmaxmark <magicmaxmark@gmail.com>
2020-03-18 16:22:08 -0400

    added scene to test stuff

---
+++ GameDevClass SampleScene/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float thrust = 50.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            rb.AddForce(new Vector3(thrust, 0,0));
+        }
+    }
+}
