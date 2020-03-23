17d0ba0048a31b1a4ad072588e6b4c060aa0bcf6
Joblez <joblezlll@gmail.com>
2020-03-23 16:36:19 -0400

    player work better
    -player taller
    -player look up and down
    -object have rigid body now

--- GameDevClass SampleScene/Assets/Scripts/MoveScript.cs
+++ GameDevClass SampleScene/Assets/Scripts/MoveScript.cs
@@ -11,8 +11,7 @@ public class MoveScript : MonoBehaviour
     public int goingForward = 0; //0 is null, 1 is true, 2 is false
     public int goingLeft = 0; //0 is null, 1 is true, 2 is false
 
-    public Transform playerBody;
-    public float mouseSensitivity;
+
 
     float xAxisClamp = 0.0f;
     // Start is called before the first frame update
@@ -26,7 +25,7 @@ public class MoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RotateCamera();
+
         Move();
     }
 
@@ -116,37 +115,6 @@ public class MoveScript : MonoBehaviour
         rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y, Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed));
     }
 
-    void RotateCamera()
-    {
-        float MouseX = Input.GetAxis("Mouse X");
-        float MouseY = Input.GetAxis("Mouse Y");
-
-        float rotAmountX = MouseX * mouseSensitivity;
-        float rotAmountY = MouseY * mouseSensitivity;
-
-        xAxisClamp -= rotAmountY;
-
-        Vector3 targetRotCam = transform.rotation.eulerAngles;
-        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
-
-        targetRotCam.x -= rotAmountY;
-        targetRotCam.z = 0;
-        targetRotBody.y += rotAmountX;
-
-        if (xAxisClamp > 90)
-        {
-            xAxisClamp = 90;
-            targetRotCam.x = 90;
-        }
-        else if (xAxisClamp < -90)
-        {
-            xAxisClamp = -90;
-            targetRotCam.x = 270;
-        }
 
 
-        transform.rotation = Quaternion.Euler(targetRotCam);
-        playerBody.rotation = Quaternion.Euler(targetRotBody);
-    }
 }
-
---
+++ GameDevClass SampleScene/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity;
+
+    float xAxisClamp = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = MouseX * mouseSensitivity;
+        float rotAmountY = MouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if (xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if (xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+    }
+}
