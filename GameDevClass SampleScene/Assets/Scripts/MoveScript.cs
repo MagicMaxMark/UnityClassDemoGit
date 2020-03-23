0200fa146b871c2c4c98e02e14127d47c009dc77
magicmaxmark <magicmaxmark@gmail.com>
2020-03-23 15:40:55 -0400

    Movescript now works with local rotation

--- GameDevClass SampleScene/Assets/Scripts/MoveScript.cs
+++ GameDevClass SampleScene/Assets/Scripts/MoveScript.cs
@@ -7,40 +7,100 @@ public class MoveScript : MonoBehaviour
     public Rigidbody rb;
     public float thrust = 50.0f;
     public float maxSpeed = 2.0f;
+
+    public int goingForward = 0; //0 is null, 1 is true, 2 is false
+    public int goingLeft = 0; //0 is null, 1 is true, 2 is false
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-            Cursor.lockState = Cursor.lockState = CursorLockMode.Locked;
-
         if (Input.GetKey(KeyCode.Escape))
             Cursor.lockState = Cursor.lockState = CursorLockMode.None;
 
+        goingForward = 0;
+        goingLeft = 0;
+
+        //if gets input and turns it into variables
         if (Input.GetKey(KeyCode.W)){
-            rb.AddForce(new Vector3(0, 0, thrust));
+            goingForward = 1;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            goingForward = 2;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(new Vector3(-thrust, 0, 0));
+            goingLeft = 1;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(new Vector3(0, 0, -thrust));
+            goingLeft = 2;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        //uses variables to determine movement
+        if (goingForward == 0)
         {
-            rb.AddForce(new Vector3(thrust, 0, 0));
+            if (goingLeft == 1)
+            {
+                rb.AddForce(transform.right * -thrust);
+            }
+
+            else if (goingLeft == 2)
+            {
+                rb.AddForce(transform.right * thrust);
+            }
         }
 
-        rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.y, -maxSpeed, maxSpeed), Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed));
+        else if (goingForward == 1)
+        {
+            if (goingLeft == 0)
+            {
+                rb.AddForce(transform.forward * thrust);
+            }
+
+            else if (goingLeft == 1)
+            {
+                rb.AddForce(transform.forward * thrust / 2);
+                rb.AddForce(transform.right * -thrust / 2);
+            }
+
+            else if (goingLeft == 2)
+            {
+                rb.AddForce(transform.forward * thrust / 2);
+                rb.AddForce(transform.right * thrust / 2);
+            }
+        }
+
+        else if (goingForward == 2)
+        {
+            if (goingLeft == 0)
+            {
+                rb.AddForce(transform.forward * -thrust);
+            }
+
+            else if (goingLeft == 1)
+            {
+                rb.AddForce(transform.forward * -thrust / 2);
+                rb.AddForce(transform.right * -thrust / 2);
+            }
+
+            else if (goingLeft == 2)
+            {
+                rb.AddForce(transform.forward * -thrust / 2);
+                rb.AddForce(transform.right * thrust / 2);
+            }
+        }
+
+        rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y, Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed));
     }
 }
